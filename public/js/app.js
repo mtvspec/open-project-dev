00f22a2098bc2f99a4de992d8ea728b3bbdfa803
mtvspec <mtv.spec@gmail.com>
2015-09-27 11:32:09 +0600

    updates login template

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 (function(){
   'use strict';
 
-
   angular.module('app', ['ui.router'])
     .config(function($stateProvider, $urlRouterProvider){
       $urlRouterProvider.otherwise('/');
@@ -17,5 +16,6 @@
           templateUrl: 'templates/tasks.html',
           controller: 'TasksCtrl'
         })
+								
     });
 })();
