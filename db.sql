ab5ab71c5e88b4291537cf89dc81e8fdda3781a6
mtvspec <mtv.spec@gmail.com>
2015-09-29 23:02:36 +0600

    adds open_project.e_tasks table and creation query to db.sql

--- db.sql
+++ db.sql
@@ -1,8 +1,17 @@
 # Сообщения
 CREATE TABLE chat.e_messages
 (
-	id 								SERIAL PRIMARY KEY,
-	user_id 					INTEGER NOT NULL REFERENCES sysinfo.users(id),
-	message_text 			TEXT NOT NULL,
-	send_date 				TIMESTAMP WITH TIME ZONE DEFAULTS LOCALTIMESTAMP NOT NULL 
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER NOT NULL REFERENCES sysinfo.users(id),
+	message_text TEXT NOT NULL,
+	send_date TIMESTAMP WITH TIME ZONE DEFAULT LOCALTIMESTAMP NOT NULL 
+);
+
+# Задачи
+CREATE TABLE open_project.e_tasks
+(
+	id SERIAL PRIMARY KEY,
+	task_name VARCHAR(200) NOT NULL UNIQUE,
+	task_description TEXT NOT NULL UNIQUE,
+	create_date TIMESTAMP WITH TIME ZONE DEFAULT LOCALTIMESTAMP NOT NULL
 );
