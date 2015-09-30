8617863e831ead31f0f0f699fb0f75f698517067
mtvspec <mtv.spec@gmail.com>
2015-09-30 23:55:16 +0600

    corrects file indention

--- public/js/controllers/TasksCtrl.js
+++ public/js/controllers/TasksCtrl.js
@@ -1,10 +1,12 @@
 (function(){
-  'use strict';
+	'use strict';
 
-  angular.module('app')
-    .controller('TasksCtrl', function($scope, TasksModel) {
-      TasksModel.getAllTasks().then(function (tasks) {
-        $scope.tasks = tasks;
-      });
-    })
+	angular.module('app')
+	.controller('TasksCtrl', function($scope, TasksModel) {
+		
+		TasksModel.getAllTasks().then(function (tasks) {
+			$scope.tasks = tasks;
+		});
+
+	})
 })();
